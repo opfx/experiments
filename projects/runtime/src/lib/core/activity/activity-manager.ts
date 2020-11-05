58b3fe2d7345df8644655a7d21678f55093e82a8
Bogdan <bogdan@Bogdans-MacBook-Pro.local>
2020-11-05 12:14:29 -0500

    wip (runtime): added initial implementation of ActivityManager

---
+++ projects/runtime/src/lib/core/activity/activity-manager.ts
@@ -0,0 +1,36 @@
+import { Injectable, Injector } from '@angular/core';
+import { Router } from '@angular/router';
+import { Intent } from './../intent';
+
+import { NavController } from './../navigation';
+
+import { Activity } from './activity';
+
+@Injectable({ providedIn: 'root' })
+export class ActivityManager {
+  private mInjector: Injector;
+  private mNavCtrl: NavController;
+  private mRouter: Router;
+
+  constructor(injector: Injector) {
+    this.mInjector = injector;
+    this.mNavCtrl = injector.get<NavController>(NavController);
+  }
+
+  public startActivity(activity: Activity, intent: Intent): void {
+    console.log(`starting activity ${intent.uri}`);
+
+    const url = intent.uri.toString().replace('content://', 'content/');
+
+    let navDirection = 'forward';
+
+    switch (navDirection) {
+      case 'forward':
+        this.mNavCtrl.navigateForward(url, { skipLocationChange: true });
+        break;
+      case 'root':
+        this.mNavCtrl.navigateRoot(url, { skipLocationChange: true });
+        break;
+    }
+  }
+}
--- projects/runtime/src/lib/core/activity/activity.ts
+++ projects/runtime/src/lib/core/activity/activity.ts
@@ -1,16 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, Injector } from '@angular/core';
 import { OnInit, OnDestroy } from '@angular/core';
+
+import { Subject } from 'rxjs';
 
 import { Intent } from './../intent';
 
+import { ActivityManager } from './activity-manager';
+
 @Component({ selector: 'wx-activity', template: `` })
 export class Activity implements OnInit, OnDestroy {
-  //   private mName: string;
+  private activityManager: ActivityManager;
 
-  public startActivity(intent: Intent): void {}
+  protected readonly destroyed = new Subject<void>();
+
+  constructor(injector: Injector) {
+    this.activityManager = injector.get<ActivityManager>(ActivityManager);
+  }
+
+  public startActivity(intent: Intent): void {
+    this.activityManager.startActivity(this, intent);
+  }
 
   public finish(): void {}
 
-  public ngOnInit(): void {}
-  public ngOnDestroy(): void {}
+  // //////////////////////////////////////////////////////
+  // Lifecycle
+
+  public ngOnInit(): void {
+    this.create();
+  }
+  public ngOnDestroy(): void {
+    this.destroy();
+  }
+
+  private create(): void {
+    this.onCreate();
+  }
+  private destroy(): void {
+    this.onDestroy();
+    this.destroyed.next();
+    this.destroyed.complete();
+  }
+
+  protected onCreate(): void {}
+  protected onDestroy(): void {}
 }
