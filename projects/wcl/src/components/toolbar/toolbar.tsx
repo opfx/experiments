d1ea63364fe3bff59b361ff74cb2765a8376e97c
Bogdan <bogdan@Bogdans-MacBook-Pro.local>
2020-11-11 09:37:55 -0500

    wip (wcl): progress on toolbar and buttons

--- projects/wcl/src/components/toolbar/toolbar.tsx
+++ projects/wcl/src/components/toolbar/toolbar.tsx
@@ -1,11 +1,29 @@
-import { Component, ComponentInterface, Host, h } from '@stencil/core';
+import { Component, ComponentInterface, Element, Host, h } from '@stencil/core';
 
 @Component({
   tag: 'wx-toolbar',
   styleUrl: './toolbar.scss',
   shadow: true,
 })
-export class ToolbarComponent implements ComponentInterface {
+export class Toolbar implements ComponentInterface {
+  @Element() el!: HTMLWxToolbarElement;
+  componentWillLoad() {
+    let buttons = Array.from(this.el.querySelectorAll('wx-buttons'));
+    const firstButtons = buttons.find((buttonsEl) => {
+      return buttonsEl.slot === 'start';
+    });
+    if (firstButtons) {
+      firstButtons.classList.add('buttons-start-slot');
+    }
+    buttons = buttons.reverse();
+    const lastButtons = buttons.find((buttonsEl) => {
+      return buttonsEl.slot === 'end';
+    });
+    if (lastButtons) {
+      lastButtons.classList.add('buttons-end-slot');
+    }
+  }
+
   render() {
     return (
       <Host>
