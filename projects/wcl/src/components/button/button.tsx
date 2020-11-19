9e00704a999ada4f81f1d21005f17fe076267419
Bogdan <bogdan@Bogdans-MacBook-Pro.local>
2020-11-19 09:37:36 -0500

    wip (wcl): restarted the button

--- projects/wcl/src/components/button/button.tsx
+++ projects/wcl/src/components/button/button.tsx
@@ -1,19 +1,17 @@
-import { Component, ComponentInterface, Element, Host, Prop, h } from '@stencil/core';
+import { Component, ComponentInterface, Element, Event, EventEmitter, Host, Prop, h } from '@stencil/core';
 
+import { AnchorInterface, ButtonInterface } from '../../api';
 import { Color, Size } from '../../api';
 import { getWxMode } from '../../core';
 import { createColorClasses } from '../../util';
 
-// See https://stenciljs.com/docs/properties
 @Component({
   tag: 'wx-button',
-  styleUrls: {
-    ios: 'button.scss',
-    md: 'button.scss',
-  },
+  styleUrl: './button.scss',
   shadow: true,
 })
-export class ButtonComponent implements ComponentInterface {
+export class Button implements ComponentInterface, AnchorInterface, ButtonInterface {
+  private inToolbar = false;
   @Element() el!: HTMLElement;
 
   /**
@@ -21,10 +19,12 @@ export class ButtonComponent implements ComponentInterface {
    */
   @Prop({ mutable: true }) buttonType = 'button';
 
-  @Prop() mode = getWxMode(this);
+  /**
+   * The color to use from your application's color palette.
+   * Default options are: `"primary"`, `"secondary"`, `"tertiary"`, `"success"`, `"warning"`, `"danger"`, `"light"`, `"medium"`, and `"dark"`.
+   * For more information on colors, see [theming](/docs/theming/basics).
+   */
   @Prop() color?: Color;
-  // @Prop({ mutable: true }) gradientname: string; // 'yellow', 'blue'; // could not set gradientType - wk-button comes with all-lower-case for camel-case
-  @Prop({ mutable: true }) gradientname = 'yellow';
 
   /**
    * If `true`, the user cannot interact with the button.
@@ -32,48 +32,148 @@ export class ButtonComponent implements ComponentInterface {
   @Prop({ reflect: true }) disabled = false;
 
   /**
+   * Set to `"block"` for a full-width button or to `"full"` for a full-width button
+   * without left and right borders.
+   */
+  @Prop({ reflect: true }) expand?: 'full' | 'block';
+
+  /**
+   * Set to `"clear"` for a transparent button, to `"outline"` for a transparent
+   * button with a border, or to `"solid"`. The default style is `"solid"` except inside of
+   * a toolbar, where the default is `"clear"`.
+   */
+  @Prop({ reflect: true, mutable: true }) fill?: 'clear' | 'outline' | 'solid' | 'default';
+
+  /**
+   * This attribute instructs browsers to download a URL instead of navigating to
+   * it, so the user will be prompted to save it as a local file. If the attribute
+   * has a value, it is used as the pre-filled file name in the Save prompt
+   * (the user can still change the file name if they want).
+   */
+  @Prop() download: string | undefined;
+
+  /**
+   * Contains a URL or a URL fragment that the hyperlink points to.
+   * If this property is set, an anchor tag will be rendered.
+   */
+  @Prop() href: string | undefined;
+
+  /**
+   * The mode determines which platform style to use.
+   */
+  @Prop({ mutable: true }) mode = getWxMode(this);
+
+  /**
+   * Specifies the relationship of the target object to the link object.
+   * The value is a space-separated list of [link types](https://developer.mozilla.org/en-US/docs/Web/HTML/Link_types).
+   */
+  @Prop() rel: string | undefined;
+
+  /**
    * The button shape.
    */
-  @Prop({ reflect: true }) shape = 'round'; // shape?: 'round';
+  @Prop({ reflect: true }) shape?: 'round';
 
   /**
    * The button size.
    */
-  @Prop({ reflect: true }) size: Size = 'default';
+  @Prop({ reflect: true }) size?: Size;
 
-  /*
-    <Host
-    ...
-    <span class="button-outer">ample</span>
-        <span class="button-inner">
-          sample
-          {/* <button class="button-native">
-            <slot></slot>
-          </button> * /}
-        </span>
-    </Host>
+  /**
+   * If `true`, activates a button with a heavier font weight.
    */
-  render() {
-    const { buttonType, gradientname, mode, size, shape } = this;
-    // console.log('button gradientname: ' + gradientname);
-    // FIX ME
-    // const TagType = href === undefined ? 'button' : ('a' as any);
-    const TagType = 'button';
+  @Prop() strong = false;
+
+  /**
+   * Specifies where to display the linked URL.
+   * Only applies when an `href` is provided.
+   * Special keywords: `"_blank"`, `"_self"`, `"_parent"`, `"_top"`.
+   */
+  @Prop() target: string | undefined;
+
+  /**
+   * The type of the button.
+   */
+  @Prop() type: 'submit' | 'reset' | 'button' = 'button';
+
+  /**
+   * Emitted when the button has focus.
+   */
+  @Event() wxFocus!: EventEmitter<void>;
+
+  /**
+   * Emitted when the button loses focus.
+   */
+  @Event() wxBlur!: EventEmitter<void>;
+
+  private get hasIconOnly(): boolean {
+    return !!this.el.querySelector('[slot="icon-only"]');
+  }
+
+  componentWillLoad(): void {
+    this.inToolbar = !!this.el.closest('wx-buttons');
+  }
+
+  private onClick = (): void => {};
+
+  private onBlur = (): void => {
+    this.wxBlur.emit();
+  };
+
+  private onFocus = (): void => {
+    this.wxFocus.emit();
+  };
+
+  render(): any {
+    const TagType = this.href === undefined ? 'button' : 'a';
+    const { expand } = this;
+    let fill = this.fill;
+    if (fill === undefined) {
+      fill = this.inToolbar ? 'clear' : 'solid';
+    }
+
+    let shape = this.shape;
+
+    let size = this.size;
+    // if(size === undefined) {
+    //   //
+    // }
+
+    const attrs =
+      TagType === 'button'
+        ? { type: this.type }
+        : { download: this.download, href: this.href, rel: this.rel, target: this.target };
     return (
       <Host
+        onClick={this.onClick}
         aria-disabled={this.disabled ? 'true' : null}
         class={createColorClasses(this.color, {
-          [mode]: true,
-          [`${buttonType}-${size}`]: true,
-          [`${buttonType}-${shape}`]: shape !== undefined,
-          [`${buttonType}-${gradientname}`]: gradientname !== undefined,
+          [this.mode]: true,
+          [this.buttonType]: true,
+          [`${this.buttonType}-${expand}`]: expand !== undefined,
+          [`${this.buttonType}-${shape}`]: shape !== undefined,
+          [`${this.buttonType}-${size}`]: size !== undefined,
+          [`${this.buttonType}-${fill}`]: true,
+          'button-has-icon-only': this.hasIconOnly,
+          'button-disabled': this.disabled,
+          'wx-activatable': true,
+          'wx-focusable': true,
         })}
       >
-        <TagType class="button-native">
-          <slot name="icon-only"></slot>
-          <slot name="start"></slot>
-          <slot></slot>
-          <slot name="end"></slot>
+        <TagType
+          {...attrs}
+          class="button-native"
+          part="native"
+          disabled={this.disabled}
+          onBlur={this.onBlur}
+          onFocus={this.onFocus}
+        >
+          <span class="button-inner">
+            <slot name="icon-only"></slot>
+            <slot name="start"></slot>
+            <slot></slot>
+            <slot name="end"></slot>
+          </span>
         </TagType>
       </Host>
     );
--- projects/wcl/src/components/button/test/button.spec.ts
+++ projects/wcl/src/components/button/test/button.spec.ts
@@ -1,26 +1,47 @@
 import { newSpecPage, SpecPage } from '@stencil/core/testing';
 import { isRegExp } from 'util';
 
-import { ButtonComponent } from '../button';
+import { Button } from './../button';
 
 describe('button', () => {
   const renderPage = async (html: string, supportsShadowDom = false): Promise<SpecPage> => {
     return await newSpecPage({
       html,
-      components: [ButtonComponent],
+      components: [Button],
       supportsShadowDom,
     });
   };
 
   it('should have shadow dom enabled', async () => {
-    const page = await renderPage('<wx-button></wx-button', true);
-    expect(page.root.shadowRoot).toBeTruthy();
+    const { root } = await renderPage('<wx-button></wx-button>', true);
+    expect(root.shadowRoot).toBeTruthy();
   });
 
   it('should have default classes', async () => {
     const page = await renderPage('<wx-button></wx-button');
-    expect(page.root.classList.contains('wx-activatable'));
-    expect(page.root.classList.contains('wx-focusable'));
+    expect(page.root.classList.contains('wx-activatable')).toBeTruthy();
+    expect(page.root.classList.contains('wx-focusable')).toBeTruthy();
+  });
+
+  it('should have the "button" class by default', async () => {
+    const { root } = await renderPage('<wx-button></wx-button>');
+    expect(root.classList.contains('button')).toBeTruthy();
+  });
+
+  it('should render with a button by default', async () => {
+    const { root } = await renderPage('<wx-button></wx-button>');
+    const firstElementChild = root.firstElementChild;
+    expect(firstElementChild.nodeName).toBe(`BUTTON`);
+  });
+
+  it('should render with an anchor when href is specified', async () => {
+    const { root } = await renderPage('<wx-button href="https://google.com"></wx-button>');
+    expect(root.firstElementChild.nodeName).toBe('A');
+  });
+
+  it('should have the "button-has-icon-only" class when an icon request the "icon-only" slot', async () => {
+    const { root } = await renderPage('<wx-button><wx-icon slot="icon-only"/></wx-button>');
+    expect(root.classList.contains('button-has-icon-only')).toBeTruthy();
   });
 
   describe('props', () => {
@@ -30,27 +51,63 @@ describe('button', () => {
       expect(page.root.hasAttribute('aria-disabled')).toEqual(false);
     });
 
-    it('should be disabled', async () => {
+    it('should be disabled when "disabled" is set to true', async () => {
       const page = await renderPage('<wx-button disabled="true"></wx-button');
       expect(page.root.hasAttribute('disabled')).toBeTruthy();
       expect(page.root.hasAttribute('aria-disabled')).toBeTruthy();
     });
 
-    describe('size', () => {
-      it('should have default size class when "size" attr is not given', async () => {
-        const page = await renderPage('<wx-button></wx-button');
-        expect(page.root.classList.contains('button-default')).toBeTruthy();
+    it('should have "button-disabled" class when "disabled" is set to true', async () => {
+      const page = await renderPage('<wx-button disabled="true"></wx-button');
+      expect(page.root.classList.contains('button-disabled')).toBeTruthy();
+    });
+
+    describe('expand', () => {
+      it('should have "button-block" class when the "expand" is set to "block"', async () => {
+        const { root } = await renderPage('<wx-button expand="block"></wx-button>');
+        expect(root.classList.contains('button-block')).toBeTruthy();
       });
 
-      it('should have specified size class when "size" attr is given', async () => {
-        const page = await renderPage(`<wx-button size='small'></wx-button`);
-        expect(page.root.classList.contains('button-small')).toBeTruthy();
-      });
-
-      it('should have specified size class when "size" attr is given and buttonType is specified', async () => {
-        const page = await renderPage(`<wx-button buttonType='some-type' size='small'></wx-button`);
-        expect(page.root.classList.contains('some-type-small')).toBeTruthy();
+      it('should have "button-full" class when the "expand" is set to "full"', async () => {
+        const { root } = await renderPage('<wx-button expand="full"></wx-button>');
+        expect(root.classList.contains('button-full')).toBeTruthy();
       });
     });
+
+    describe('fill', () => {
+      it('should have "button-solid" class by default', async () => {
+        const { root } = await renderPage('<wx-button></wx-button>');
+        expect(root.classList.contains('button-solid')).toBeTruthy();
+      });
+
+      it('should have "button-clear" class when the "fill" is set to "clear"', async () => {
+        const { root } = await renderPage('<wx-button fill="clear"></wx-button>');
+        expect(root.classList.contains('button-clear')).toBeTruthy();
+      });
+
+      it('should have "button-outline" class when the "fill" is set to "outline"', async () => {
+        const { root } = await renderPage('<wx-button fill="outline"></wx-button>');
+        expect(root.classList.contains('button-outline')).toBeTruthy();
+      });
+    });
+
+    describe('shape', () => {
+      it('should have "button-round" class when the "shape" is set to "round"', async () => {
+        const { root } = await renderPage('<wx-button shape="round"></wx-button>');
+        expect(root.classList.contains('button-round')).toBeTruthy();
+      });
+    });
+  });
+
+  describe('events', () => {
+    // it('should emit "wxFocus" event when receives focus', async () => {
+    //   const _cb = jest.fn();
+    //   const page = await renderPage('<wx-button></wx-button>');
+    //   page.doc.addEventListener('wxFocus', _cb);
+    //   await page.waitForChanges();
+    //   const p = page.root.querySelector('button');
+    //   p.focus();
+    //   expect(_cb).toHaveBeenCalled();
+    // });
   });
 });
