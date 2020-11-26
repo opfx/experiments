cf8348c9401b18b331c4288a2fa01203e4bcc100
Bogdan <bogdan@Bogdans-MacBook-Pro.local>
2020-11-26 02:46:16 -0500

    chore (play): corrected zone-flags and index

--- projects/play/src/zone-flags.ts
+++ projects/play/src/zone-flags.ts
@@ -1,5 +1,11 @@
 // disable patching requestAnimationFrame
 (window as any).__Zone_disable_requestAnimationFrame = true;
 
+/**
+ * Prevents Angular change detection from
+ * running with certain Web Component callbacks
+ */
+(window as any).__Zone_disable_customElements = true;
+
 // disable patching specified eventNames
 // (window as any).__zone_symbol__UNPATCHED_EVENTS = ['scroll', 'mousemove'];
