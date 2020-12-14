2dcc62b2156aab16140c6457364f66e6d83c1395
sergenik <snikiforovski@gmail.com>
2020-12-14 18:59:05 -0500

    chore(wcl + cash-cow): set stage container and loop function in pixi component

--- projects/cash-cow/src/lib/cash-cow.component.ts
+++ projects/cash-cow/src/lib/cash-cow.component.ts
@@ -1,20 +1,45 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, AfterViewInit, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { Container, Loader, Renderer, Text, Ticker, utils } from 'pixi.js';
+import { WxPixi } from 'projects/runtime/src/lib/wcl/components/proxies';
 
 @Component({
   selector: 'wx-cash-cow',
-  template: `<wx-pixi></wx-pixi> `,
+  template: `<wx-pixi stage="this.stage" loop="this.loop"></wx-pixi>`,
   styles: [],
 })
-export class CashCowComponent implements OnInit, OnDestroy {
-  constructor(private router: Router) {}
+export class CashCowComponent implements AfterViewInit, OnInit, OnDestroy {
+  @ViewChild(WxPixi, { static: false }) pixi: WxPixi;
+  private player: WxPixi;
+  private stage: Container;
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
-    console.log('Cash cow init');
+    this.init();
+    this.stage = new Container();
+  }
+
+  ngAfterViewInit(): void {
+    const defaultText = new Text('Cash-Cow', { fontFamily: 'Webkinz Medium', fontSize: 50 });
+    defaultText.position.set(50, 50);
+    this.stage.addChild(defaultText);
+
+    this.pixi.stage = this.stage;
+    this.pixi.loop = this.loop;
   }
 
   ngOnDestroy(): void {
-    console.log('Cash cow destroy');
+    console.log('[cash-cow].destroy');
+  }
+
+  private init(): void {
+    const loader = Loader.shared;
+    const webGLSupported = utils.isWebGLSupported();
+    // const target = utils.CanvasRenderTarget;
+  }
+
+  public loop(delta: number): void {
+    // console.log('[cash-cow] delta: ' + delta);
   }
 
   public close(): void {
